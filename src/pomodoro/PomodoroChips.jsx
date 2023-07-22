0e50dd40bcdd9c9c8e9c9d4338ec0d4fd77458f3
ALBANO-Daniel <deanidev@gmail.com>
2023-07-22 19:25:36 +0200

    change chips to apple shape, pomodoro i.e.

--- src/pomodoro/PomodoroChips.jsx
+++ src/pomodoro/PomodoroChips.jsx
@@ -1,24 +1,60 @@
-import { Chip, Stack } from '@mui/material'
+import { Box, Typography, useTheme } from '@mui/material'
 import React from 'react'
+import PomodoroIcon from '../assets/images/pomme.png'
+import PomodoroIconWhite from '../assets/images/pommeWhite.png'
+// import PomodoroFinishedIcon from '../assets/images/pommeEnd2.png'
+import PomodoroFinishedIcon from '../assets/images/appleEnd.png'
+import PomodoroFinishedIconWhite from '../assets/images/appleEndWhite.png'
 
 function PomodoroChips({ index }) {
   return (
     <React.Fragment>
-      <Stack direction="row" gap={2} paddingBottom={1} >
-        <Chip label="work" color="success" />
-        <Chip label="pause" color="success" variant={index < 1 ? 'outlined' : ''} />
-        <Chip label="work" color="success" variant={index < 2 ? 'outlined' : ''} />
-        <Chip label="pause" color="success" variant={index < 3 ? 'outlined' : ''} />
-      </Stack>
-      <Stack direction="row" gap={2}>
-        <Chip label="work" color="success" variant={index < 4 ? 'outlined' : ''} />
-        <Chip label="pause" color="success" variant={index < 5 ? 'outlined' : ''} />
-        <Chip label="work" color="success" variant={index < 6 ? 'outlined' : ''} />
-        <Chip label="pause" color="error" variant={index < 7 ? 'outlined' : ''} />
-      </Stack>
+      <Box direction="row" display="flex" gap={2} paddingBottom={1}>
+        <PomodoroChip label="work" color="green" finished={index < 1 ? false : true} />
+        <PomodoroChip label="pause" color="salmon" finished={index < 2 ? false : true} />
+        <PomodoroChip label="work" color="green" finished={index < 3 ? false : true} />
+        <PomodoroChip label="pause" color="salmon" finished={index < 4 ? false : true} />
+      </Box>
+      <Box direction="row" gap={2} display="flex">
+        <PomodoroChip label="work" color="green" finished={index < 5 ? false : true} />
+        <PomodoroChip label="pause" color="salmon" finished={index < 6 ? false : true} />
+        <PomodoroChip label="work" color="green" finished={index < 7 ? false : true} />
+        <PomodoroChip label="pause" color="red" finished={false} />
+      </Box>
     </React.Fragment>
   )
 }
 
 export default PomodoroChips
+
+export const PomodoroChip = (props) => {
+  const { label, color, finished } = props
+
+  const theme = useTheme()
+  const mode = theme.palette.mode
+
+  return finished ? (
+    <Box color={color} sx={{ position: 'relative', opacity: '0.3' }}>
+      {mode === 'dark' ? (
+        <img src={PomodoroFinishedIconWhite} alt="stage icon" width="50px" height="45px" />
+      ) : (
+        <img src={PomodoroFinishedIcon} alt="stage icon" width="50px" height="45px" />
+      )}
+      <Typography color={color} sx={{ position: 'absolute', top: 0, paddingX: 1, paddingTop: 2, fontSize: '11px' }}>
+        {label}
+      </Typography>
+    </Box>
+  ) : (
+    <Box color={color} sx={{ position: 'relative' }}>
+      {mode === 'dark' ? (
+        <img src={PomodoroIconWhite} alt="stage icon" width="50px" height="45px" />
+      ) : (
+        <img src={PomodoroIcon} alt="stage icon" width="50px" height="45px" />
+      )}
+      <Typography color={color} sx={{ position: 'absolute', top: 0, paddingX: 1.2, paddingTop: 1.5 }}>
+        {label}
+      </Typography>
+    </Box>
+  )
+}
 
